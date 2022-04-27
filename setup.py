8fc8c6ae52b17ee0beaa0c0ba7e89d6d4d83f34d
Meir Komet <mskomet1@gmail.com>
2022-04-27 22:38:41 +0300

    env: Added setup.py for packaging
    Signed-off-by: Meir Komet <mskomet1@gmail.com>

---
+++ setup.py
@@ -0,0 +1,84 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="rubedo",  # Required
+    version="0.0.0",  # Required
+    description="Abstract data backends via dataclasses",  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        # Pick your license as you wish
+        "License :: OSI Approved ::  License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="sql dataclasses backend",  # Optional
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),  # Required
+    python_requires=">=3.8, <4",
+    dependency_links=[],
+    install_requires=[
+        "attrs==21.4.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "cached-property==1.5.2",
+        "cerberus==1.3.4",
+        "certifi==2021.10.8",
+        "chardet==4.0.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "charset-normalizer==2.0.12; python_version >= '3'",
+        "colorama==0.4.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "distlib==0.3.4",
+        "greenlet==1.1.2; python_version >= '3' and platform_machine == 'aarch64' or (platform_machine == 'ppc64le' or (platform_machine == 'x86_64' or (platform_machine == 'amd64' or (platform_machine == 'AMD64' or (platform_machine == 'win32' or platform_machine == 'WIN32')))))",
+        "idna==3.3; python_version >= '3'",
+        "orderedmultidict==1.0.1",
+        "packaging==20.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "pep517==0.12.0",
+        "pip-shims==0.7.0; python_version >= '3.6'",
+        "pipenv-setup==3.2.0",
+        "pipfile==0.0.2",
+        "platformdirs==2.5.2; python_version >= '3.7'",
+        "plette[validation]==0.2.3; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "pyparsing==2.4.7; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "python-dateutil==2.8.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "requests==2.27.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5'",
+        "requirementslib==1.6.4; python_version >= '3.7'",
+        "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "sqlalchemy==1.4.36",
+        "toml==0.10.2; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "tomli==2.0.1; python_version >= '3.6'",
+        "tomlkit==0.10.2; python_version >= '3.6' and python_version < '4.0'",
+        "urllib3==1.26.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4.0'",
+        "vistir==0.5.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        "wheel==0.37.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+    ],  # Optional
+    extras_require={"dev": []},  # Optional
+    project_urls={  # Optional
+        "Source": "https://github.com/pypa/sampleproject/",
+    },
+)
