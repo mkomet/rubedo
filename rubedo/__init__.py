d516fb950fbbb34154f4310e24957894e9feafe6
Meir Komet <mskomet1@gmail.com>
2022-05-04 00:17:35 +0300

    groups: Added group for exposing models
    Exposing models with relation to their hierarchy,
    with extensive comparisons and aggregations.
    
    Signed-off-by: Meir Komet <mskomet1@gmail.com>

--- rubedo/__init__.py
+++ rubedo/__init__.py
@@ -1,7 +1,9 @@
 from .enhanced_fields import Indexed, NonNullable, PrimaryKey, Unique
+from .group import Group
 from .model import rubedo_model
 from .model_base import ModelBase
 from .repository_base import RepositoryBase, ViewBase
+from .search import SearchResults
 
 __all__ = [
     "rubedo_model",
@@ -12,4 +14,6 @@ __all__ = [
     "Unique",
     "PrimaryKey",
     "NonNullable",
+    "Group",
+    "SearchResults",
 ]
